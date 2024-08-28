314eb7135eef5fcf27affb75cdb2b2e7e5a0c693
Daniil <dan.ponyavin@yandex.ru>
2024-08-28 21:50:32 +0300

    Created Dockerfile

--- pkg/storage/postgres.go
+++ pkg/storage/postgres.go
@@ -1,9 +1,11 @@
 package storage
 
 import (
+	"fmt"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"log"
+	"os"
 )
 
 type PostgresStorage struct {
@@ -11,7 +13,15 @@ type PostgresStorage struct {
 }
 
 func NewPostgresStorage() *PostgresStorage {
-	connStr := "host=localhost port=5428 user=postgres password=mypass dbname=test sslmode=disable"
+	host := os.Getenv("POSTGRES_HOST")
+	if host == "" {
+		host = "localhost"
+	}
+	port := os.Getenv("POSTGRES_PORT")
+	if port == "" {
+		port = "5428"
+	}
+	connStr := fmt.Sprintf("host=%s port=%s user=postgres password=mypass dbname=test sslmode=disable", host, port)
 	db, err := sqlx.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
